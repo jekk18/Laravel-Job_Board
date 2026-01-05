9a5240766391aacf2956314c560e2af1fb9c789e
jekk18 <chelidzejeko9123741jeko@gmail.com>
2026-01-05 21:39:21 +0400

    start project

---
+++ app/Models/OfferedJob.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OfferedJob extends Model
+{
+    /** @use HasFactory<\Database\Factories\OfferedJobsFactory> */
+    use HasFactory;
+ 
+    public static array $experience = ['entry', 'intermediate', 'senior'];
+    public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+}
